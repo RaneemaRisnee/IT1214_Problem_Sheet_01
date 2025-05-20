86c4df0dee3b018a182462c65601a88934abea92
Raneema Risnee <144576507+RaneemaRisnee@users.noreply.github.com>
2025-05-20 16:30:08 +0000

    01 updates

--- 01.java
+++ 01.java
@@ -1,9 +1,21 @@
 class Student{
-    String name;
-    int marks;
+    private int studentId;
+    private String name;
+    private int dayAttended;
 
-    public int getMarks() {
-        return marks;
+    Student(int studentId,String name,int dayAttended){
+        this.studentId = studentId;
+        this.name = name;
+        this.dayAttended = dayAttended;
+    }
+    public int getStudentId() {
+        return studentId;
+    }
+    public int getName() {
+        return name;
+    }
+    public int getDayAttended() {
+        return dayAttended;
     }
 
 public static void main(String [] args){
